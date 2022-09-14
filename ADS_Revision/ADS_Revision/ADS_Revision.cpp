dcfeecf9af2c089d5216e5ffbc714d4fcacda78d
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-09-14 16:55:29 +0100

    Added exception example to divide

--- ADS_Revision/ADS_Revision/ADS_Revision.cpp
+++ ADS_Revision/ADS_Revision/ADS_Revision.cpp
@@ -3,12 +3,22 @@
 using namespace std;
 
 //function declaration
-int add(int x, int y);
+int add(int x, int y); //semi-colon
+double divide(double x, double y);
 
 int main()
 {
     //functions - add, divide
     cout << add(3, 4) << endl;
+    try {
+        cout << divide(3, 0) << endl;
+    }
+    catch (invalid_argument e) {
+        cout << e.what() << endl;
+    }
+    catch (...) {
+        cout << "A bad thing happened!";
+    }
 
     //move functions into header
     //look at swap - ptrs, reference, refs to pointers
@@ -20,8 +30,13 @@ int add(int x, int y)
     return x + y;
 }
 
-float divide(float x, float y)
+double divide(double x, double y)
 {
+    if (y == 0) throw invalid_argument("Invalid denominator!");
+   /* if (y == 0)
+        throw "Invalid denominator!";*/
+ /*   if (y == 0)
+        return 0;*/
     return x / y;
 }
 
